8edd65c261483e09d4873f01552f3ac7f5b46d31
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-08-25 07:57:18 +0530

    useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import AuthApp from "./AuthApp";
-import UnAuthApp from "./UnAuthApp";
-import { useContext } from "react";
-import { MyLoginContext } from "./LoginContext";
+import { useReducer } from "react";
 
+const App = () => {
+  const colorMethod = (state, action) => {
+    switch (action) {
+      case "red":
+        return "red";
+      case "green":
+        return "green";
+      case "blue":
+        return "blue";
+      case "yellow":
+        return "yellow";
+      default:
+        return state;
+    }
+  };
 
-const App=()=>{
-    const { user } = useContext(MyLoginContext);
-    return(
-        <>
-        <h1>Login system</h1>
-        {user.auth? <AuthApp/> :<UnAuthApp/> }
-        </>
+  const [color, dispatch] = useReducer(colorMethod, "violet");
 
-    )
-}
+  return (
+    <>
+      <button onClick={() => dispatch("red")}>Red</button>
+      <button onClick={() => dispatch("blue")}>Blue</button>
+      <button onClick={() => dispatch("green")}>Green</button>
+      <button onClick={() => dispatch("yellow")}>Yellow</button>
+
+      <div
+        style={{
+          backgroundColor: color,
+          width: "300px",
+          height: "200px",
+          marginTop: "10px",
+        }}
+      ></div>
+    </>
+  );
+};
 
 export default App;
